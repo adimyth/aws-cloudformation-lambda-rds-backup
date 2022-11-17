db66d292e2431812771d031732cf6aa30ff7e631
adimyth <aditya@fnp.dev>
2022-11-17 21:05:25 +0530

    fixes lambda-deploy.sh

--- pipeline/lambda-deploy.sh
+++ pipeline/lambda-deploy.sh
@@ -15,17 +15,19 @@ execOnExist() {
 
 execOnNotExist() {
     # Create lambda function
-    lambda=$(aws lambda create-function --function-name "$FUNCTION_NAME" --role "$LAMBDA_ROLE" --code "ImageUri=$IMAGE_URI" --package-type Image --region "$AWS_REGION" --vpc-config "SubnetIds=subnet-093ddff71b52b991d,subnet-0e077ea1638249135,SecurityGroupIds=sg-06ef17860bccbda25,sg-0ab932b6a62e816b2" --timeout 30 --memory-size 1024 --publish | jq --raw-output '.FunctionArn')
-    echo "\n\n"
+    lambdarnid=$(aws lambda create-function --function-name "$FUNCTION_NAME" --role "$LAMBDA_ROLE" --code "ImageUri=$IMAGE_URI" --package-type Image --region "$AWS_REGION" --vpc-config "SubnetIds=subnet-093ddff71b52b991d,subnet-0e077ea1638249135,SecurityGroupIds=sg-06ef17860bccbda25,sg-0ab932b6a62e816b2" --timeout 30 --memory-size 1024 --publish | jq --raw-output '.FunctionArn')
+    echo -e "\n\n"
     echo "Lambda Function ARN: "$lambdarnid
 
     # Create S3 bucket to store the database dump files
-    aws s3 mb s3://"$BUCKET_NAME"
+    s3arnid=$(aws s3 mb s3://"$BUCKET_NAME" --output text | grep "arn" | tr -d '"')
+    echo -e "\n\n"
+    echo "S3 ARN: "$s3arnid
 
     # Create event rule to schedule a cron expression every 12 hours
-    rule=$(aws events put-rule --name "$RULE_NAME" --description "Event rule to schedule a cron expression every 12 hours" --schedule-expression 'rate(12 hours)' --output text | grep "arn" | tr -d '"')
-    echo "\n\n"
-    echo "Event Rule ARN: "$rule
+    rulearnid=$(aws events put-rule --name "$RULE_NAME" --description "Event rule to schedule a cron expression every 12 hours" --schedule-expression 'rate(12 hours)' --output text | grep "arn" | tr -d '"')
+    echo -e "\n\n"
+    echo "Event Rule ARN: "$rulearnid
 
     # Grant permission to the rule to allow it to trigger the lambda function
     aws lambda add-permission --function-name "$FUNCTION_NAME" --statement-id "$EVENT_NAME" --action 'lambda:InvokeFunction' --principal events.amazonaws.com --source-arn arn:aws:events:ap-south-1:223865955266:rule/"$RULE_NAME"
